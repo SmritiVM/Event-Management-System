2c653e8cf2aaa1c8d0f2bd4a9dc4ddf7c6480c73
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-11-08 10:33:38 +0530

    Update registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,10 @@
-// import { HashRouter, Routes, Route } from 'react-router-dom';
-
-// import Navbar from './components/Navbar/Navbar';
-// import Footer from "./components/Footer/Footer";
-// import Home from './components/Home/Home';
-// import Register from "./components/Login/Register";
-// import RegistrationForm from './components/Login/Register2';
-
-// import './App.css';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <HashRouter>
-//         <Navbar/>
-//         <Routes>
-//           <Route path = "/" element = {<Home/>}/>
-//           <Route path = "/register" element = {<Register/>}/>
-        
-//         </Routes>
-//         <Footer/>
-//       </HashRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Footer from "./components/Footer/Footer";
 import Home from './components/Home/Home';
-import Register from "./components/Login/Register";
-// import EventRegistrationForm from './components/Events/Eventsform';
+// import Register from "./components/Login/Register";
+import RegistrationForm from './components/Login/Register';
 
 import './App.css';
 
@@ -47,7 +16,8 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path = "/" element = {<Home/>}/>
-          <Route path = "/register" element = {<Register/>}/>
+          <Route path = "/register" element = {<RegistrationForm/>}/>
+        
         </Routes>
         <Footer/>
       </HashRouter>
@@ -56,4 +26,3 @@ function App() {
 }
 
 export default App;
-
--- frontend/src/components/Login/Register.js
+++ frontend/src/components/Login/Register.js
@@ -1,177 +1,108 @@
-// import $ from "jquery";
+import React, { useState } from 'react';
 
-export default function Register(){
-    
-    // $(document).ready(function () {
-    //     $('#contact_form').bootstrapValidator({
-    //         feedbackIcons: {
-    //             valid: 'glyphicon glyphicon-ok',
-    //             invalid: 'glyphicon glyphicon-remove',
-    //             validating: 'glyphicon glyphicon-refresh'
-    //         },
-    //         fields: {
-    //             User_name: {
-    //                 validators: {
-    //                     stringLength: {
-    //                         min: 8,
-    //                         message: 'Please enter at least 8 characters for User Name'
-    //                     },
-    //                     notEmpty: {
-    //                         message: 'Please supply your User Name'
-    //                     }
-    //                 }
-    //             },
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    college: '',
+    event: '',
+  });
 
-    //             Full_name: {
-    //                 validators: {
-    //                     stringLength: {
-    //                         min: 2,
-    //                         message: 'Please enter at least 2 characters for Full Name'
-    //                     },
-    //                     notEmpty: {
-    //                         message: 'Please supply your Full Name'
-    //                     }
-    //                 }
-    //             },
-                
-    //             email: {
-    //                 validators: {
-    //                     notEmpty: {
-    //                         message: 'Please enter your Email Address'
-    //                     },
-    //                     emailAddress: {
-    //                         message: 'Please enter a valid Email Address'
-    //                     }
-    //                 }
-    //             },
-    //             phone: {
-    //                 validators: {
-    //                     notEmpty: {
-    //                         message: 'Please enter your Phone Number'
-    //                     },
-    //                     stringLength: {
-    //                         min: 10,
-    //                         max: 12,
-    //                         message: 'Phone number should be between 10 and 12 digits'
-    //                     }
-    //                 }
-    //             },
-    //             password: {
-    //                 validators: {
-    //                     notEmpty: {
-    //                         message: 'Please enter a password'
-    //                     },
-    //                     stringLength: {
-    //                         min: 8,
-    //                         message: 'Password should be at least 8 characters'
-    //                     },
-    //                     regexp: {
-    //                         regexp: /^(?=.*[A-Z])(?=.*\d).+$/,
-    //                         message: 'Password must contain at least one capital letter and one number'
-    //                     }
-    //                 }
-    //             },
-    //             confirm_password: {
-    //                 validators: {
-    //                     notEmpty: {
-    //                         message: 'Please confirm your password'
-    //                     },
-    //                     identical: {
-    //                         field: 'password',
-    //                         message: 'The password and its confirmation must match'
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     })
-    //         .on('success.form.bv', function (e) {
-    //             // Prevent form submission
-    //             e.preventDefault();
-    //         });
-    // });
-    return(
-        <div class="container">
-        <form class="well form-horizontal" action="" method="post" id="contact_form">
-            <fieldset>
-               
-                <div class="form-group">
-                    <label class="col-md-4 control-label">User Name</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="User_name" placeholder="User Name" class="form-control" type="text"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Full Name</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="Full_name" placeholder="Full Name" class="form-control" type="text"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
-                
-                <div class="form-group">
-                    <label class="col-md-4 control-label">E-Mail</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                            <input name="email" placeholder="E-Mail Address" class="form-control" type="text"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Phone #</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                            <input name="phone" placeholder="(845)555-1212" class="form-control" type="text"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here, e.g., send data to a server.
+    console.log(formData);
+  };
 
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="userName">Username:</label>
+          <input
+            type="text"
+            id="userName"
+            name="userName"
+            value={formData.userName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="fullName">Full Name:</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="repassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="repassword"
+            name="repassword"
+            value={formData.repassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Create Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input name="password" placeholder="Create Password" class="form-control" type="password"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
-                
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Confirm Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input name="confirm_password" placeholder="Confirm Password" class="form-control" type="password"/>
-                        </div>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                </div>
-                
-            
-                
-                <div class="form-group">
-                    <label class="col-md-4 control-label"></label>
-                    <div class="col-md-4">
-                        <button type="submit" class="btn btn-warning">Submit <span
-                                class="glyphicon glyphicon-send"></span></button>
-                    </div>
-                </div>
-            </fieldset>
-        </form>
+        {/* <div>
+          <label htmlFor="event">Select Event:</label>
+          <select
+            id="event"
+            name="event"
+            value={formData.event}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select an event</option>
+            <option value="Event A">Event A</option>
+            <option value="Event B">Event B</option>
+            <option value="Event C">Event C</option>
+          </select>
+        </div> */}
+        <button type="submit">Register</button>
+      </form>
     </div>
-    )
-}
+  );
+};
+
+export default RegistrationForm;
+
+
+
+

eab907e7ca86ee1e3c5872a4684da02d2ae44902
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2025-05-08 12:55:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb+srv://jayp_mishra:1234mish@cluster0.7eeb128.mongodb.net/emsdb");
+mongoose.connect("<DATABASE NAME>");
 var db = mongoose.connection;
 db.on("open", () => console.log("Connected to DB"));
 db.on("error", () => console.log("Error occurred"));
